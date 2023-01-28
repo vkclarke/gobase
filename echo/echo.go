86933c191c3b61d6afb61fef59a91c2d4d4a8978
Vincent Clarke <vincent@windows97.space>
2023-01-28 00:34:06 -0500

    somehow I did not catch this

--- echo/echo.go
+++ echo/echo.go
@@ -7,12 +7,11 @@ import (
 	"strings"
 )
 
-flag.Usage = func() {
-	name := os.Args[0][strings.LastIndexAny(os.Args[0], "/\\")+1:]
-	fmt.Fprintf(os.Stderr, "Usage: %s [-n] text\n", name)
-}
-
 func main() {
+	flag.Usage = func() {
+		name := os.Args[0][strings.LastIndexAny(os.Args[0], "/\\")+1:]
+		fmt.Fprintf(os.Stderr, "Usage: %s [-n] text\n", name)
+	}
 	n := flag.Bool("n", false, "suppress newline")
 	flag.Parse()
 	fmt.Print(strings.Join(flag.Args(), " "))
